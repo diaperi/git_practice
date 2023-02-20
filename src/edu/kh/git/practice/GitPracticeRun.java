8ed869172bbd924868b803429e359ae48e987786
SeoYun <diaperi0901@gmail.com>
2023-02-20 19:16:16 +0900

    첫번째 커밋 생성

---
+++ src/edu/kh/git/practice/GitPracticeRun.java
@@ -0,0 +1,10 @@
+package edu.kh.git.practice;
+
+public class GitPracticeRun {
+
+	public static void main(String[] args) {
+		System.out.println("안녕하세요");
+		System.out.println("Git연습용 파일 입니다.");
+	}
+
+}
